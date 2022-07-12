9d2426260c2d07a440bd0f1ecb0eea3d0ac24550
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2022-07-12 11:07:11 +0530

    Complete Create DBConnection

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentFormController {
+    public AnchorPane studentContext;
+    public TableView tblStudent;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtContact;
+    public JFXTextField txtEmail;
+    public JFXTextField txtNIC;
+    public JFXTextField txtAddress;
+
+    public void studentIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
 }
