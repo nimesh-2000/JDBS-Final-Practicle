5267cafd74bd6dd80130ed42791f3074935c3938
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2022-07-12 11:13:32 +0530

    Complete Student Model

---
+++ src/model/Student.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    String student_id;
+    String student_name;
+    String email;
+    String contact;
+    String nic;
+}
